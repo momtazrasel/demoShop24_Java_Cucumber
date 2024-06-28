e9c716fe19898223f5b43e82658e78ebc40fa6ff
momtazrasel <momtazrasel@gmail.com>
2024-06-28 12:17:02 +0600

    Project is updated

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -11,8 +11,8 @@ public class Hooks {
         this.driver = driverSetup.driver;
     }
 
-//    @After
-//    public void tearDown() {
-//        driver.quit();
-//    }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 }
